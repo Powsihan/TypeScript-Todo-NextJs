692fc62f75a7bc6ec64848fbb08e7a38603914e9
Powsihan <powsipowsihan07@gmail.com>
2024-08-19 13:15:43 +0530

    middleware part added

---
+++ typescript-todo/src/middleware.tsx
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { cookies } from "next/headers";
+
+
+const protectedRoutes = [
+    "/home",
+];
+const publicRoutes = ["/"];
+
+export default async function middleware(req: NextRequest) {
+ 
+    const path = req.nextUrl.pathname;
+    const isProtectedRoute = protectedRoutes.includes(path);
+    const isPublicRoute = publicRoutes.includes(path);
+
+   
+    const cookie = cookies().get("user")?.value;
+    
+    if (isProtectedRoute && !cookie) {
+        return NextResponse.redirect(new URL("/", req.nextUrl));
+    }
+
+   
+    if (isPublicRoute && cookie && !req.nextUrl.pathname.startsWith("/home")) {
+        return NextResponse.redirect(new URL("/home", req.nextUrl));
+    }
+
+    return NextResponse.next();
+}
+
+
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
